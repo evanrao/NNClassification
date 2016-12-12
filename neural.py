0cee4460288b3068e14c497ec20b5b417250e158
evanrao <yw_rao@sina.com>
2016-12-13 00:06:40 +0800

    Update neural.py

--- neural.py
+++ neural.py
@@ -5,7 +5,7 @@ class Neural:
         self.Si = si #The number of samples
         self.Xi = xi # Enter the number of variables
         self.Ui = ui # Enter the number of membership functions for each input variable
-        # Hi <= Ui ^ Xi ; Hidden layer refers to the number of rules here, Rules, the number of hidden nodes
+        # Hi <= Ui ** Xi ; Hidden layer refers to the number of rules here, Rules, the number of hidden nodes
         self.Hi = self.Ui ** self.Xi
         self.Oi = oi # The number of output nodes
 
