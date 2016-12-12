55de45a6d68dd718f740d270158a8f136db3b73d
evanrao <yw_rao@sina.com>
2016-12-12 22:37:15 +0800

    Add files via upload

---
+++ matstorage.py
@@ -0,0 +1,31 @@
+# coding=gbk
+import numpy as np
+from scipy import io
+
+
+def dataload(filename):
+    data =  np.load(filename)
+    #print(data)
+    return data
+
+# Here, the data is normalized between 0 and 1 by using the logarithmic process of 10 for the sample
+def log_normalization(data):
+    colnum = len(data[0])
+    rownum = len(data)
+    #print data[0:len(data),0]
+    nordata = np.zeros((rownum, colnum))
+    for i in range(0, colnum):
+        tempi = np.log10(data[0:rownum,i])
+        tempi = (tempi - np.min(tempi)) / (np.max(tempi) - np.min(tempi));
+        nordata[0:rownum,i] = tempi;
+        #print ('-----------'+ '%d' %i + '-----------')
+    return nordata
+
+def mattonpz(srcname,srckey,outname):
+    in_d = io.loadmat(srcname)
+    oct_a = in_d[srckey]
+    oct_a = float(oct_a)
+    np.save(outname,oct_a)
+
+if __name__ == '__main__':
+    print log_normalization(dataload('in_data.npy'))
